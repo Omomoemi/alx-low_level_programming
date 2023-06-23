64d1e54849239057d96b72139f5ae9eb88140ef5
Omomoemi <emiagbozu@gmail.com>
2023-06-23 03:44:35 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
- * main - A program that prints all digits in base 16
+ * main - A programm that prints digits in base 16
  *
- * Return: 0 (succes)
+ * Return: 0 (success)
  */
 int main(void)
 {
 	int c;
 	char letter;
+
 	for (c = 0; c < 10; c++)
 		putchar((c % 10) + '0');
-	for (letter = 'a'; letter <= 'f'; letter++)
+	for (letter = 'a'; letter < 'f'; letter++)
 		putchar(letter);
 	putchar('\n');
 	return (0);
 }
-	

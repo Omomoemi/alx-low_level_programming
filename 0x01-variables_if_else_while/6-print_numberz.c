6dd9cbfcb55adec1b74a420cb2c940ea6422c769
Omomoemi <emiagbozu@gmail.com>
2023-06-23 02:33:49 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all single digits from 10 using putchar twice
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for(i=0; i<10; i++)
+		putchar((i % 10) + '0');
+	putchar('\n');
+	return (0);
+}

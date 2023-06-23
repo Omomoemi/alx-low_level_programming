186f264a3ce387a2b0a15f54f91bcfc2240d9fbc
Omomoemi <emiagbozu@gmail.com>
2023-06-23 01:55:09 -0700

    4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabets in lowercase followed by a line
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return(0);
+}

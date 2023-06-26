af80f53cca234b0ff93649622c9971104d39d661
Omomoemi <emiagbozu@gmail.com>
2023-06-25 23:39:56 -0700

    0-reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - A function that takes a pointer to an int
- *@*n: the pointer to variable
+ * reset_to_98 - A function that takes a pointer to an int
+ *@n: the pointer to variable
  */
 
 void reset_to_98(int *n)

3c14f08366ed177e3251234d5c84c735f34bfee8
Omomoemi <emiagbozu@gmail.com>
2023-07-03 10:30:35 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,3 +21,22 @@ char *_strstr(char *haystack, char *needle)
 	}
 	return (0);
 }
+/**
+ * coincedence - define if the string b is in a
+ * @a: source string
+ * @b: string to be searched
+ * 
+ * Return: 1 if there is coincidence, otherwise 0.
+ */
+int coincidence(char *a, char *b)
+{
+	while (*b && *b == *a)
+	{
+		b++;
+		a++;
+	}
+	if (*b == '\0')
+		return (1);
+	else
+		return (0);
+}

544fc07cc2be2ab68c521dc78ede032b7a692f0c
Omomoemi <emiagbozu@gmail.com>
2023-07-05 02:27:09 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - A function that prints a string followed by a new line
+ * @s: the string to print
+ */
+void _puts_recursion(char *s)
+{
+	int s;
+
+	prinf(s; '%d\n', s)
+		return 0;
+}
